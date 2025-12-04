52822b427088306a203991ec932a8121f7f5aa37
amr mohamed <amr332763@gmail.com>
2025-12-04 20:11:40 +0200

    icons Done

--- frontend/src/components/Header_Footer/Footer/page.jsx
+++ frontend/src/components/Header_Footer/Footer/page.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import "./Footer.css";
 import logoImage from "../../../assets/loopFooter.png";
 
+// Font Awesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faXTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
   const [toast, setToast] = useState({ show: false, message: "" });
 
@@ -20,55 +25,47 @@ function Footer() {
                   <img src={logoImage} alt="Loop logo" className="logo-image" />
                 </div>
               </div>
+
               <p>
-                Egypt's premier recycling platform. Making sustainability
-                profitable for everyone.
+                Egypt's premier recycling platform. Making sustainability profitable for everyone.
               </p>
-              <div className="social-links">
-                <a
-                  href="#"
-                  aria-label="Facebook"
-                  title="Facebook"
-                  className="social-link"
-                >
-                  <i className="fab fa-facebook"></i>
-                </a>
-              </div>
+<div className="social-links">
+  <a href="#" aria-label="Facebook" title="Facebook" className="social-link">
+    <FontAwesomeIcon icon={faFacebook} />
+  </a>
+
+  <a href="#" aria-label="Instagram" title="Instagram" className="social-link">
+    <FontAwesomeIcon icon={faInstagram} />
+  </a>
+
+  <a href="#" aria-label="X (Twitter)" title="X" className="social-link">
+    <FontAwesomeIcon icon={faXTwitter} />
+  </a>
+
+  <a href="#" aria-label="LinkedIn" title="LinkedIn" className="social-link">
+    <FontAwesomeIcon icon={faLinkedin} />
+  </a>
+</div>
+
             </div>
 
             <div className="footer-col">
               <h4>Quick Links</h4>
               <ul>
-                <li>
-                  <a href="#home">Home</a>
-                </li>
-                <li>
-                  <a href="#calculator">Calculator</a>
-                </li>
-                <li>
-                  <a href="#how">How it Works</a>
-                </li>
-                <li>
-                  <a href="#impact">Our Impact</a>
-                </li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#calculator">Calculator</a></li>
+                <li><a href="#how">How it Works</a></li>
+                <li><a href="#impact">Our Impact</a></li>
               </ul>
             </div>
 
             <div className="footer-col">
               <h4>Materials</h4>
               <ul>
-                <li>
-                  <a href="#">Metal Recycling</a>
-                </li>
-                <li>
-                  <a href="#">Paper Recycling</a>
-                </li>
-                <li>
-                  <a href="#">Plastic Recycling</a>
-                </li>
-                <li>
-                  <a href="#">Pricing Guide</a>
-                </li>
+                <li><a href="#">Metal Recycling</a></li>
+                <li><a href="#">Paper Recycling</a></li>
+                <li><a href="#">Plastic Recycling</a></li>
+                <li><a href="#">Pricing Guide</a></li>
               </ul>
             </div>
 
@@ -76,13 +73,13 @@ function Footer() {
               <h4>Contact</h4>
               <ul>
                 <li>
-                  <i className="fas fa-phone"></i> 01234567890
+                  <FontAwesomeIcon icon={faPhone} /> 01234567890
                 </li>
                 <li>
-                  <i className="fas fa-envelope"></i> info@karakib.com
+                  <FontAwesomeIcon icon={faEnvelope} /> info@karakib.com
                 </li>
                 <li>
-                  <i className="fas fa-map-marker-alt"></i> Cairo, Egypt
+                  <FontAwesomeIcon icon={faMapMarkerAlt} /> Cairo, Egypt
                 </li>
               </ul>
             </div>
@@ -99,7 +96,6 @@ function Footer() {
         </div>
       </footer>
 
-      {/* Toast Notification */}
       {toast.show && (
         <div className="toast" id="toast">
           <div className="toast-content">
