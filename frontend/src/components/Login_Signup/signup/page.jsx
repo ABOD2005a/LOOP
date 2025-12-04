ce3e694322fcab686d08f71671f8937768d8699f
ABOD <mr.abod2005a@gmail.com>
2025-12-04 19:19:52 +0200

    add fortawesome react

--- frontend/src/components/Login_Signup/signup/page.jsx
+++ frontend/src/components/Login_Signup/signup/page.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Eye, EyeOff, Check } from "lucide-react";
 import "./SignUp.css";
-import "../../Header_Footer/Navbar/page";
 import Navbar from "../../Header_Footer/Navbar/page";
 
 export default function SignUp() {
