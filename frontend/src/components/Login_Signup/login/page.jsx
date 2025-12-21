73ff5a1fb761697494d311ed2b67bbacaf85b2af
ABOD <mr.abod2005a@gmail.com>
2025-12-21 23:11:56 +0200

    some edit in NavbarAfter and

--- frontend/src/components/Login_Signup/login/page.jsx
+++ frontend/src/components/Login_Signup/login/page.jsx
@@ -73,7 +73,7 @@ export default function Login() {
         const data = await response.json();
 
         if (response.ok) {
-
+          localStorage.setItem("userId", data.user.id.toString());
           localStorage.setItem("user", JSON.stringify(data.user));
 
           if (formData.rememberMe) {
